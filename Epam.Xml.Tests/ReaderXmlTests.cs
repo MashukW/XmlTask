e456c9e111708ce2c74d4455a475b572e1d295bf
Vadim Mashuk <MashukW@gmail.com>
2016-04-07 19:43:33 +0300

    ImplementXmlReaderAndWriteTests

--- Epam.Xml.Tests/ReaderXmlTests.cs
+++ Epam.Xml.Tests/ReaderXmlTests.cs
@@ -1,13 +1,64 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace Epam.Xml.Tests
 {
     [TestFixture]
     public class ReaderXmlTests
     {
-        [Test]
-        public void TestMethod1()
+        private ReaderXml _readerXml;
+        private string _xmlRoute = "TestXML.xml";
+        private string _xPath = "//author";
+        private Dictionary<string, int> _actualDictionryElement;
+        
+        [OneTimeSetUp]
+        public void ReaderXmlTestsInit()
         {
+            if (_readerXml == null)
+                _readerXml = new ReaderXml(_xmlRoute, _xPath);
+
+            _actualDictionryElement = new Dictionary<string, int>
+            {
+                {"Gambardella, Matthew", 1},
+                {"Ralls, Kim", 1},
+                {"Corets, Eva", 3},
+                {"Randall, Cynthia", 1},
+                {"Thurman, Paula", 1},
+                {"Knorr, Stefan", 1},
+                {"Kress, Peter", 1},
+                {"O'Brien, Tim", 2},
+                {"Galos, Mike", 1}
+            };
+        }
+
+        [TestCase("TestXML.xml", "")]
+        [TestCase("", "//author")]
+        public void CreateObjectTypeOfReaderXml_UsingNullOrEmptyXmlRouteOrReaderXml_ExpectedArgumentNullException(
+            string route,
+            string xPathExp)
+        {
+            Assert.That(() => new ReaderXml(route, xPathExp),
+                Throws.InstanceOf(typeof (ArgumentNullException)));
+        }
+
+        [Test]
+        public void GetElementsDictionary_UsingCorrectXmlRouteAndReaderXml_ExpectedValidDictionaryElements()
+        {
+            var expectedDictionaryElement = _readerXml.GetElementsDictionary();
+
+            CollectionAssert.AreEquivalent(expectedDictionaryElement, _actualDictionryElement);
+        }
+
+        [Test]
+        public void GetElementsDictionary_UsingInvalidPathToXml_ExpectedArgumentException()
+        {
+            string incorrectPathToXml = "D:/Install";
+
+            ReaderXml readerWithInvalidPath = new ReaderXml(incorrectPathToXml, _xPath);
+
+            Assert.That(()=> readerWithInvalidPath.GetElementsDictionary(),
+                Throws.InstanceOf(typeof(ArgumentException)));
         }
     }
 }
--- Epam.Xml/ReaderXml.cs
+++ Epam.Xml/ReaderXml.cs
@@ -1,7 +1,57 @@
-﻿namespace Epam.Xml
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.XPath;
+
+namespace Epam.Xml
 {
     public class ReaderXml
     {
+        private string _xmlRoute;
+        private string _xPath;
+        
+        public ReaderXml(string xmlRoute, string xPath)
+        {
+            if (string.IsNullOrWhiteSpace(xmlRoute))
+                throw new ArgumentNullException(nameof(xmlRoute));
 
+            if (string.IsNullOrWhiteSpace(xPath))
+                throw new ArgumentNullException(nameof(xPath));
+
+            _xmlRoute = xmlRoute;
+            _xPath = xPath;
+        }
+        
+        public Dictionary<string, int> GetElementsDictionary()
+        {
+            XPathDocument document;
+            Dictionary<string, int> result = new Dictionary<string, int>();
+            
+            try
+            {
+                document = new XPathDocument(_xmlRoute);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                throw new ArgumentException(e.Message + " или указан неправильный путь!");
+            }
+
+            XPathNavigator navigator = document.CreateNavigator();
+            XPathNodeIterator nodes = navigator.Select(_xPath);
+
+            while (nodes.MoveNext())
+            {
+                AddToDictionary(result, nodes.Current);
+            }
+
+            return result;
+        }
+
+        private void AddToDictionary(Dictionary<string, int> dictionary, XPathItem node)
+        {
+            if (dictionary.ContainsKey(node.Value))
+                dictionary[node.Value]++;
+            else
+                dictionary.Add(node.Value, 1);
+        }
     }
 }
